e76c2fed206026029bd37f225126126c15fa9f75
Brad Sickles <brad.sickles@gmail.com>
2020-08-21 11:04:47 -0400

    Fixing bugs

--- cert.tf
+++ cert.tf
@@ -11,17 +11,23 @@ resource "aws_acm_certificate" "this" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  for_each = aws_acm_certificate.this.domain_validation_options
+  for_each = {
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-  name            = each.value["resource_record_name"]
+  name            = each.value.name
   type            = "CNAME"
   allow_overwrite = true
   zone_id         = data.terraform_remote_state.domain.outputs.zone_id
-  records         = [each.value["resource_record_value"]]
+  records         = [each.value.record]
   ttl             = 60
 }
 
 resource "aws_acm_certificate_validation" "this" {
   certificate_arn         = aws_acm_certificate.this.arn
-  validation_record_fqdns = aws_route53_record.cert_validation.*.fqdn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
